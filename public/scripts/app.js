fc12af1363dd8bd5dece5b0cf30ad49ab8138094
Akash Arora <akasharora@Akashs-MacBook-Air.local>
2021-01-15 23:02:50 +0530

    Set up react and babel

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,9 @@
-console.log('HI');
+'use strict';
+
+var template = React.createElement(
+  'h1',
+  null,
+  'Indecision App'
+);
+var appRoot = document.getElementById('app');
+ReactDOM.render(template, appRoot);
---
+++ src/app.js
@@ -0,0 +1,3 @@
+var template = <h1>Indecision App</h1>;
+var appRoot = document.getElementById('app');
+ReactDOM.render(template, appRoot);
