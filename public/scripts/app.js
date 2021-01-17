e915e6cd1671410bddc75b8b8f45ad72b05b510f
Akash Arora <akasharora@Akashs-MacBook-Air.local>
2021-01-17 19:46:55 +0530

    makes dynamic code

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,9 +1,39 @@
 'use strict';
 
-var template = React.createElement(
-  'h1',
-  null,
-  'Indecision App'
+// const template = (
+//     <div>
+//         <h1>My name is YoDaddy</h1>
+//         <p>Hi, this is some information</p>
+//     </div>
+// );
+
+var user = {
+    name: 'Akash Arora',
+    age: 20,
+    location: 'Faridabad, Haryana'
+};
+
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        user.name.toUpperCase()
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: ',
+        user.age
+    ),
+    user.location && React.createElement(
+        'p',
+        null,
+        'Location: ',
+        user.location
+    )
 );
+
 var appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,3 +1,23 @@
-var template = <h1>Indecision App</h1>;
-var appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
+// const template = (
+//     <div>
+//         <h1>My name is YoDaddy</h1>
+//         <p>Hi, this is some information</p>
+//     </div>
+// );
+
+const user = {
+    name: 'Akash Arora',
+    age: 20,
+    location: 'Faridabad, Haryana'
+};
+
+const templateTwo = (
+    <div>
+        <h1>{user.name.toUpperCase()}</h1>
+        <p>Age: {user.age}</p>
+        {user.location && <p>Location: {user.location}</p>}
+    </div>
+);
+
+const appRoot = document.getElementById('app');
+ReactDOM.render(templateTwo, appRoot);
