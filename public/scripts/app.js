8dd2d2d1e1999267a359d699fd9d4ffa1b5b5a57
Akash Arora <akasharora@Akashs-MacBook-Air.local>
2021-05-18 17:35:04 +0530

    Adds basic logic

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,39 +1,87 @@
 'use strict';
 
-// const template = (
-//     <div>
-//         <h1>My name is YoDaddy</h1>
-//         <p>Hi, this is some information</p>
-//     </div>
-// );
+var app = {
+    title: 'Indecision',
+    subtitle: 'Put your life in the hands of a computer',
+    options: []
+};
+var onFormSubmit = function onFormSubmit(e) {
+    e.preventDefault();
+    var option = e.target.elements.option.value;
 
-var user = {
-    name: 'Akash Arora',
-    age: 20,
-    location: 'Faridabad, Haryana'
+    if (option) {
+        app.options.push(option);
+        renderOptions();
+        e.target.elements.option.value = '';
+    };
 };
 
-var templateTwo = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        user.name.toUpperCase()
-    ),
-    React.createElement(
-        'p',
-        null,
-        'Age: ',
-        user.age
-    ),
-    user.location && React.createElement(
-        'p',
-        null,
-        'Location: ',
-        user.location
-    )
-);
+var onRemoveAll = function onRemoveAll() {
+    app.options = [];
+    renderOptions();
+};
+
+var onMakeDecision = function onMakeDecision() {
+    var len = app.options.length;
+    if (!len) return null;
+    var randomOption = Math.trunc(Math.random() * 10) % len;
+    alert(app.options[randomOption]);
+};
 
 var appRoot = document.getElementById('app');
-ReactDOM.render(templateTwo, appRoot);
+var renderOptions = function renderOptions() {
+    var numberOfOptions = app.options.length;
+    var template = React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'h1',
+            null,
+            app.title
+        ),
+        app.subtitle && React.createElement(
+            'p',
+            null,
+            app.subtitle
+        ),
+        React.createElement(
+            'p',
+            null,
+            numberOfOptions > 0 ? 'Here are your options:' : 'No options'
+        ),
+        React.createElement(
+            'button',
+            { disabled: !app.options.length, onClick: onMakeDecision },
+            'Make My Decision!'
+        ),
+        numberOfOptions > 0 ? React.createElement(
+            'button',
+            { onClick: onRemoveAll },
+            'Remove All'
+        ) : null,
+        numberOfOptions ? React.createElement(
+            'ol',
+            null,
+            app.options.map(function (option, i) {
+                return React.createElement(
+                    'li',
+                    { key: i },
+                    option
+                );
+            })
+        ) : null,
+        React.createElement(
+            'form',
+            { onSubmit: onFormSubmit },
+            React.createElement('input', { type: 'text', name: 'option' }),
+            React.createElement(
+                'button',
+                null,
+                'Add Option'
+            )
+        )
+    );
+    ReactDOM.render(template, appRoot);
+};
+
+renderOptions();
--- src/app.js
+++ src/app.js
@@ -1,23 +1,52 @@
-// const template = (
-//     <div>
-//         <h1>My name is YoDaddy</h1>
-//         <p>Hi, this is some information</p>
-//     </div>
-// );
-
-const user = {
-    name: 'Akash Arora',
-    age: 20,
-    location: 'Faridabad, Haryana'
+const app = {
+    title: 'Indecision',
+    subtitle: 'Put your life in the hands of a computer',
+    options: []
+};
+const onFormSubmit = (e) => {
+    e.preventDefault();
+    const { target: { elements: { option: { value: option } } } } = e;
+    if (option) {
+        app.options.push(option);
+        renderOptions();
+        e.target.elements.option.value = '';
+    };
 };
 
-const templateTwo = (
-    <div>
-        <h1>{user.name.toUpperCase()}</h1>
-        <p>Age: {user.age}</p>
-        {user.location && <p>Location: {user.location}</p>}
-    </div>
-);
+const onRemoveAll = () => {
+    app.options = [];
+    renderOptions();
+};
+
+const onMakeDecision = () => {
+    const len = app.options.length;
+    if (!len)
+        return null;
+    const randomOption = Math.trunc(Math.random() * 10) % len;
+    alert(app.options[randomOption]);
+};
 
 const appRoot = document.getElementById('app');
-ReactDOM.render(templateTwo, appRoot);
+const renderOptions = () => {
+    const numberOfOptions = app.options.length;
+    const template = (
+        <div>
+            <h1>{app.title}</h1>
+            {app.subtitle && <p>{app.subtitle
+            }</p>}
+            <p>{numberOfOptions > 0 ? 'Here are your options:' : 'No options'}</p>
+            <button disabled={!app.options.length} onClick={onMakeDecision}>Make My Decision!</button>
+            {numberOfOptions > 0 ? <button onClick={onRemoveAll}>Remove All</button> : null}
+            {numberOfOptions ? <ol>
+                {app.options.map((option, i) => <li key={i}>{option}</li>)}
+            </ol> : null}
+            <form onSubmit={onFormSubmit}>
+                <input type="text" name="option"></input>
+                <button>Add Option</button>
+            </form>
+        </div>
+    );
+    ReactDOM.render(template, appRoot);
+};
+
+renderOptions();
